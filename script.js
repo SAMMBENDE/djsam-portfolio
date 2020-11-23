c06f45e5659529dd906d3c5645408d2ffcfe2a93
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2020-11-23 13:28:48 +0100

    modified lang and added audio

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Hot"],
+        strings: ["DjSaM ?"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
